6c27d5c08b7c18e7b5901a9a02fd1acfcb2b7e5e
THIGSHICCA <thigshiccavignesh@gmail.com>
2025-10-07 14:37:24 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import '@/styles/globals.css';
-
 import { PropsWithChildren } from 'react';
 import type { Metadata } from 'next';
+
+import '@/styles/globals.css';
 
 import { ActiveSectionProvider } from '@/components/active-section-provider';
 import { ThemeProvider } from '@/components/theme-provider';
@@ -44,4 +44,4 @@ const RootLayout = ({ children }: PropsWithChildren) => {
   );
 };
 
-export default RootLayout;  
+export default RootLayout;
