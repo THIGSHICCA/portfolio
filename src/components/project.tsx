7ec9651054607afe15001fecfbdacd1ba4538feb
THIGSHICCA <thigshiccavignesh@gmail.com>
2025-10-07 14:28:14 +0530

    update

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import Image from 'next/image';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
+
 import { projectsData } from '@/lib/data';
 
 type TProject = (typeof projectsData)[number];
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
+
 import { skillsData } from '@/lib/data';
 
 const fadeInAnimationVariants = {
